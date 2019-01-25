b0192f8f5319c25a34a91fd69fd6751aa35ef3f4
victorx716 <victorliu716@gmail.com>
2019-01-24 20:38:07 -0800

    Fulfills the basic exercises for promises

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -8,16 +8,42 @@
  * HINT: We exported some similar promise-returning functions in previous exercises
  */
 
-var fs = require('fs');
 var Promise = require('bluebird');
-
+var fs = Promise.promisifyAll(require('fs'));
+var getGithub = require('./promisification.js');
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
+
+  return fs.readFileAsync(readFilePath, 'utf-8')
+    .then((data) => {
+      return data.split('\n').shift();
+    })
+    .then( (oneLine) => {
+      return getGithub.getGitHubProfileAsync(oneLine)
+    })
+    .then( (user) => {
+      console.log('STRING ME FAM', JSON.stringify(user))
+      return fs.writeFileAsync(writeFilePath, JSON.stringify(user))
+    })
+    .catch( (err) => {
+      console.log('THIS DID NOT WORK!')
+    })
 };
 
 // Export these functions so we can test them
 module.exports = {
   fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
 };
+
+
+//  return new Promise(function(resolve, reject) {
+//     fs.readFile(filePath, 'utf-8', (err, data) => {
+//       if (data) {
+//         resolve(data.split('\n')[0]);
+//       } else {
+//         reject(err);
+//       }
+//     });
+//   });
